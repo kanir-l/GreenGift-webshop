fab1c795521704796ed46184966377fd84655bf6
kanir-l <kr@kanir.se>
2021-01-10 16:15:48 +0100

    modules

--- js/store.js
+++ js/store.js
@@ -1,3 +1,8 @@
+import { cart } from './cart.js';
+import { cartActive } from './cart.js';
+import { createCartProducts } from './cart.js';
+import { notice } from './cart.js';
+
 class Product {
     constructor(image, price, name, description) {
       this.id = id++;
@@ -26,7 +31,6 @@ let savedProducts = JSON.parse(localStorage.getItem("savedProducts"));
 if (savedProducts) {
     cartProducts = savedProducts
 } 
-
 
 
 let product1 = new Product(
@@ -69,17 +73,13 @@ let product6 = new Product(
 $(function () {
     listProducts()
     createProducts()
-    cart()
-    createCartProducts()
-    cartActive()
-    notice()
 })
 
-function listProducts() {
+export function listProducts() {
    products.push(product1, product2, product3, product4, product5, product6)
 }
 
-function createProducts() {
+export function createProducts() {
     let productList = $('#product-list').addClass('p-list')
     
     $.each(products, (i, product) => {
@@ -97,7 +97,7 @@ function createProducts() {
     })
 }
 
-function clickAddProducts(e) {
+export function clickAddProducts(e) {
     let existingCartProduct = cartProducts.filter((item) => {
         return item.product_id === e.data.p.id;
     });
@@ -115,103 +115,6 @@ function clickAddProducts(e) {
 
     createCartProducts()
     notice()
-}
-
-function cart() {
-    let cartContainer = $('#cart-container').addClass('cart-container')
-
-    $('<h3>').addClass('total-price').html("Total Price:" + " " + totalPrice() ).appendTo(cartContainer)
-
-    $('<button>').addClass('purchase-button').html("Continue to Payment").appendTo(cartContainer)
-}
-
-function createCartProducts() {
-    
-    let cartList = $('#cart-list').addClass('c-list')
-
-    cartList.html("")
-
-    $.each(cartProducts, (i, cartProduct) => {
-
-        let cartID = $('<div>').addClass('c-id').attr('id', cartProduct.id).appendTo(cartList)
-
-        $('<div>').addClass('c-image').html(cartProduct.image).appendTo(cartID)
-        $('<h3>').addClass('c-name').html(cartProduct.name).appendTo(cartID)
-        $('<h2>').addClass('c-price').html(cartProduct.price).appendTo(cartID)
-
-        $('<h2>').addClass('c-amount').html(cartProduct.amount).appendTo(cartID)
-
-        let amountPlus = $('<button>').addClass('c-plus').html("+").appendTo(cartID)
-        amountPlus.on('click', {plus: cartProduct}, plusAmount)
-
-        let amountMinus = $('<button>').addClass('c-minus').html("-").appendTo(cartID)
-        amountMinus.on('click', {minus: cartProduct, cartIndex: i}, minusAmount)    
-
-        let deleteFromCart = $('<button>').addClass('delete-from-cart').html("Delete").appendTo(cartID)
-        deleteFromCart.on('click', {c: i}, clickDeleteCartProducts) 
-        // sending 'i' as the index of the array. cannot sending in the whole thing as cartProduct[i] 
-        // because that contains a whole thing as an object in an array. Splice works with array, means just the index
-    })
-    
-    $('.total-price').html("Total Price:" + " " + totalPrice() )
-}
-
-function cartActive() {
-    $('#basket').on('click', function() {
-        if($('#cart-container').hasClass('cart-container')) {
-            $('#cart-container').removeClass('cart-container').addClass('cart-container-active').fadeIn(1000)
-        }
-        else {
-            $('#cart-container').removeClass('cart-container-active').addClass('cart-container')
-        }
-    })
-}
-
-function clickDeleteCartProducts(e) {
-    cartProducts.splice(e.data.c, 1)
-    console.log(cartProducts)
-
-    localStorage.setItem("savedProducts", JSON.stringify(cartProducts));
-
-    createCartProducts()
-    notice()
-}
-
-function plusAmount(e) {
-    e.data.plus.amount++;
-
-    localStorage.setItem("savedProducts", JSON.stringify(cartProducts));
-    
-    createCartProducts()
-    notice()
-}
-
-function minusAmount(e) {
-     e.data.minus.amount--;
-    if (e.data.minus.amount === 0) {
-        cartProducts.splice(e.data.cartIndex, 1)
-    } 
-
-    localStorage.setItem("savedProducts", JSON.stringify(cartProducts));
-
-    createCartProducts()
-    notice()
-}
-
-function totalPrice() {
-    let sum = 0
-    cartProducts.forEach((item, i) => {
-        sum += item.price * item.amount
-    })
-
-    return sum
-}
-
-function notice() {
-    let set = cartProducts.reduce(function (a, b){
-        return a + b.amount}, 0) 
-      
-    $('.notice').html(set)
 }
 
 
